b14199e6682a1fad733652cbd1643462a9be9c05
Zbyněk Poulíček <poulicek@gmail.com>
2021-04-05 16:17:07 +0200

    GraphicsHelper extension

--- Source/Helpers/GraphicsHelper.cs
+++ Source/Helpers/GraphicsHelper.cs
@@ -10,10 +10,18 @@ namespace TrayToolkit.Helpers
         public static void SetHighQuality(this Graphics g)
         {
             g.SmoothingMode = SmoothingMode.HighQuality;
+            g.CompositingQuality = CompositingQuality.HighQuality;
             g.InterpolationMode = InterpolationMode.HighQualityBicubic;
             g.TextRenderingHint = TextRenderingHint.ClearTypeGridFit;
         }
 
+        public static void SetLowQuality(this Graphics g)
+        {
+            g.SmoothingMode = SmoothingMode.HighSpeed;
+            g.CompositingQuality = CompositingQuality.HighSpeed;
+            g.InterpolationMode = InterpolationMode.NearestNeighbor;
+            g.TextRenderingHint = TextRenderingHint.SystemDefault;
+        }
 
         public static Size Ceiling(this SizeF s)
         {
